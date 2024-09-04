c03c8a9dc2ade45db871b06c97f48c488552e729
MaksymZhabchyk <zabcikmaksim@gmail.com>
2024-09-04 19:49:20 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+m = "Hello World"
+first_n = "Zaba"
+last_n= "Krutelik"
+age = 20
+
+
+print("Змінна 'm':", type(m), "Значення:", m)
+print("Змінна 'first_n'", type(first_n), "Значення:", first_n)
+print("Змінна 'last_n':", type(last_n), "Значення:", last_n)
+print("Змінна 'age':", type(age), "Значення:", age)
